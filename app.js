d6cde6dc47325ca232771ef5623c17bd1fbdccbc
ferrodri <f.rodriguez.hervias@gmail.com>
2017-07-17 20:23:55 +0200

    signup done

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const app = require('express')()
-const passport = require('passport')
+const app = require('express')();
+const passport = require('passport');
 
-require('dotenv').load()
-require('./config/passport')(passport)
-require('./config/express')(app)
+require('dotenv').load();
+require('./config/passport')(passport);
+require('./config/express')(app);
 
-const index = require('./routes/index')
-const verse = require('./routes/verse')
-const auth = require('./routes/auth')
+const index = require('./routes/index');
+const verse = require('./routes/verse');
+const auth = require('./routes/auth');
 
-app.use('/', index)
-app.use('/verse', verse)
-app.use('/auth', auth)
+app.use('/', index);
+app.use('/verse', verse);
+app.use('/auth', auth);
 
-require('./config/error-handler')(app)
-module.exports = app
+require('./config/error-handler')(app);
+module.exports = app;
