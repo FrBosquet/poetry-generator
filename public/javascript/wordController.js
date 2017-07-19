1caf2a65915622ae8f309e55db9b93444c2daa0a
Fran Bosquet <frbosquet@gmail.com>
2017-07-19 15:35:50 +0200

    Front word controller

--- public/javascript/wordController.js
+++ public/javascript/wordController.js
@@ -15,11 +15,38 @@ $(document).ready(()=>{
         $('#formContent').val('');
       });
     });
-    $(":button[wordid='detail596f29112b8609063c63b955']").on('click', (()=>{
-      console.log("hola botones");
-      // $.ajax({
-      // url: `${this.BASE_URL}/characters`,
-      // method: "GET",
-      // });
-    }));
+
+    let formConten = $('#formContent');
+
+    $("#formSelect").on('change', (e)=>{
+       let selectValue = $(e.target).val();
+       let placeHolder;
+
+       switch(selectValue){
+         case 'who':
+          placeHolder = 'Algo como "damisela"';
+         break;
+         case 'verb':
+          placeHolder = 'Algo como "desearía"';
+         break;
+         case 'adj':
+          placeHolder = 'Algo como "bonita"';
+         break;
+         case 'what':
+          placeHolder = 'Algo como "quererte"';
+         break;
+         case 'how':
+          placeHolder = 'Algo como "fuerte"';
+         break;
+         case 'where':
+          placeHolder = 'Algo como "en un bosque"';
+         break;
+         case 'when':
+          placeHolder = 'Algo como "toda la noche"';
+         break;
+       }
+
+       formConten.attr('placeholder', placeHolder);
+     });
+
 });
